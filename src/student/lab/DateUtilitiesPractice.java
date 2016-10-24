a3dde0ae540dbef9dd86071db6949af22c0fb0b8
Ryan <Ryan@192.168.0.18>
2016-10-24 16:08:07 -0500

    Merge origin/master
    Conflicts:
    	nbproject/project.properties

---
+++ src/student/lab/DateUtilitiesPractice.java
@@ -0,0 +1,46 @@
+package student.lab;
+
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.Year;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Date;
+import java.time.Period;
+
+public class DateUtilitiesPractice {
+
+    
+    
+    public void payDay(){
+        LocalDate today = LocalDate.now();
+        LocalDate payday = today.with(TemporalAdjusters.lastDayOfMonth());
+        System.out.println("Paday is on: " + payday.format(DateTimeFormatter.ofPattern("M/d/yy")));
+         
+    }
+    
+    public void timeTillBirthday(){
+        LocalDate today = LocalDate.now();
+        LocalDate bDay = today.of(1996, Month.AUGUST, 21);
+        LocalDate nextBDay = bDay.withYear(today.getYear());
+        
+        if(nextBDay.isBefore(today) || nextBDay.isEqual(today)){
+            nextBDay = nextBDay.plusYears(1);
+        }
+        
+        Period p = Period.between(today, nextBDay);
+        long p2 = ChronoUnit.DAYS.between(today, nextBDay);
+        System.out.println("There are " + p.getMonths() + " months, and "+ p.getDays() + " days until your next birthday. (" + p2 + " total days)");
+        
+        
+    }
+
+    
+    
+}
---
+++ src/student/lab/Run.java
@@ -0,0 +1,14 @@
+package student.lab;
+
+
+public class Run {
+
+    public static void main(String[] args) {
+        
+        DateUtilitiesPractice dup = new DateUtilitiesPractice();
+        
+        dup.payDay();
+        dup.timeTillBirthday();
+    }
+    
+}
